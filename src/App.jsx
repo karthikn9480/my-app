cc104600af0a3f4bce211853ea641f418fefa46b
karthikn9480 <71779380+karthikn9480@users.noreply.github.com>
2025-02-28 14:13:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>My AWS Task</h1>
+      <h1>My AWS Task V1</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
